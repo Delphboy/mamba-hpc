fc9a4e98f54855777198f4178226f129d23abca8
Henry <hps1945@live.co.uk>
2025-06-16 15:49:01 +0100

    UPDATE: Added mamba 1

--- test.py
+++ test.py
@@ -17,3 +17,14 @@ if __name__ == "__main__":
     y = model(x)
     assert y.shape == x.shape
     print(x, "-->", y)
+
+    model = Mamba(
+        # This module uses roughly 3 * expand * d_model^2 parameters
+        d_model=dim, # Model dimension d_model
+        d_state=16,  # SSM state expansion factor
+        d_conv=4,    # Local convolution width
+        expand=2,    # Block expansion factor
+    ).to("cuda")
+    y = model(x)
+    assert y.shape == x.shape
+    print(x, "-->", y)
