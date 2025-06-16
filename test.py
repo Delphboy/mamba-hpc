813da1ba8239d0ab4bcf3ce7015fb9a46169b8ca
Henry <hps1945@live.co.uk>
2025-06-16 15:42:55 +0100

    FIX: works on andrena and added Mamba2 support

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import torch
 from mamba_ssm import Mamba
+from mamba_ssm import Mamba2
 
 if __name__ == "__main__":
     print("Running mamba!")
 
-    batch, length, dim = 2, 64, 16
+    batch, length, dim = 2, 64, 512
     x = torch.randn(batch, length, dim).to("cuda")
-    model = Mamba(
+    model = Mamba2(
         # This module uses roughly 3 * expand * d_model^2 parameters
         d_model=dim, # Model dimension d_model
         d_state=16,  # SSM state expansion factor
